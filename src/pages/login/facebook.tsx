7e4abfe90d5f246f750669c1107a25f668b1b8d8
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2024-05-26 04:06:31 -0300

    refact: facebook

--- src/pages/login/facebook.tsx
+++ src/pages/login/facebook.tsx
@@ -1,23 +1,19 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { setItem } from "../../utils/local-storage";
 
-// Componente para lidar com o redirecionamento de volta do Facebook
 const FacebookCallbackHandler = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Verifique se a URL possui o token de acesso
     console.log(`################################`)
     const searchParams = new URLSearchParams(location.search);
     const token = searchParams.get("accessToken");
     if (token) {
-      // Armazene o token no localStorage
-      localStorage.setItem("accessToken", token);
-      // Redirecione para a página principal
+      setItem("accessToken", token);
       navigate("/");
     } else {
-      // Em caso de falha, redirecione para a página de login
       navigate("/login");
     }
   }, [location, navigate]);
